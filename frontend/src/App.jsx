cadce70f68cc52b5562f845bca0a01d2818b2308
Shreyash-Bhimte <shreyash.bhimte99@gmail.com>
2026-06-08 21:15:45 +0530

    add React Router with Home and Favourites routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
+import Favourites from './pages/Favourites'
+import { Routes, Route } from 'react-router-dom' 
 
 // function App() {
 //   return (
@@ -28,12 +30,24 @@ import Home from './pages/Home'
 //   )
 // }
   
+////////////////////
+// function App() {
+//   return (
+//   <>  
+//     <Home />
+//   </>
+//   )
+// }
+//////////////////
 
 function App() {
   return (
-  <>
-    <Home />
-  </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </main>
   )
 }
 
