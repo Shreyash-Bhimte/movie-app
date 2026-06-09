44c90fee89200335651a940a0597386bf10c65ec
Shreyash-Bhimte <shreyash.bhimte99@gmail.com>
2026-06-09 10:06:07 +0530

    add NavBar component with Home and Favourites links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
 import Favourites from './pages/Favourites'
 import { Routes, Route } from 'react-router-dom' 
+import NavBar from './components/NavBar'
 
 // function App() {
 //   return (
@@ -42,12 +43,16 @@ import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favourites" element={<Favourites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
+    </div>
+    
   )
 }
 
